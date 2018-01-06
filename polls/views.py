b9a1609ecbe38b4a30b21587624cb9b2868c0577
unknown <cobety0305@gmail.com>
2018-01-06 17:02:49 +0900

    하드코딩된URL경로를 url태그를 사용하여 결합도 낮추기

--- polls/views.py
+++ polls/views.py
@@ -1,26 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+# from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.shortcuts import render
+from django.http import Http404
 
 from django.template import loader
 
 # Create your views here
 def index(request):
+    # render()사용
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+    # template 사용
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+
+    #
     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # output = ', '.join([q.question_text for q in latest_question_list])
     # return HttpResponse(output)
     # return HttpResponse("Hello, world! ")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # 404 error with get_object_or_404 단축키 활용해서 exception처리
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+    # # 404 error
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'polls/detail.html', {'question': question})
+    #
+    # return HttpResponse("You're looking at question %s." % question_id)
 
     # return HttpResponse("detail page using HttpResponse")
+
+
 
 def results(request, question_id):
     response = "Yor're looking at the results of question %s."
